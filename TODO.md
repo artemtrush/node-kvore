31118cf571b0e3dae60c17f0b12145e026ac9cca
atrush <atrush.main@gmail.com>
2022-03-27 12:58:03 +0300

    Setup base folders structure

---
+++ TODO.md
@@ -0,0 +1,6 @@
+1. Choose package name +
+2. Create directories for adapters +
+3. Setup kernel with memory adapter
+4. Create tests with benchmarks (jest)
+5. Apply github action CI/CD
+6. Publish packages
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const a = {
+    test : 'aaa',
+    lll  : 14
+};
+
+console.log(a);
+
+function asdadasdkadskadssssssssssssssssssssssssssssssssssss(aaaa?: string) {
+    console.log(aaaa);
+}
+
+asdadasdkadskadssssssssssssssssssssssssssssssssssss();
